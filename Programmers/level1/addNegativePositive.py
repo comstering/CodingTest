04c2813750ab271ff8641730427cb5a577908d0d
comstering <coms1101@naver.com>
2021-05-14 21:56:58 +0900

    Copy the Code of Monthly Code Challenge Season 2 to normal directory

---
+++ Programmers/level1/addNegativePositive.py
@@ -0,0 +1,12 @@
+def solution(absolutes, signs):
+    answer = 0
+    for num, sign in zip(absolutes, signs):
+        if sign:
+            answer += num
+        else:
+            answer -= num
+    return answer
+
+
+print(solution([4, 7, 12], [True, False, True]))    # 9
+print(solution([1, 2, 3], [False, False, True]))    # 0
