8528efee62649677287dc3277cd926a4684abce7
comstering <coms1101@naver.com>
2021-05-17 15:39:01 +0900

    'Matching Remove' Problem of level2 success in Programmers

---
+++ Programmers/level2/matchingRemove.py
@@ -0,0 +1,13 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if len(stack) > 0 and stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    return 1 if len(stack) == 0 else 0
+
+
+
+print(solution('baabaa'))    # 1
+print(solution('cdcd'))    # 0
