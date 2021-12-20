cf3f94f5e437184d9706cf5e9a2f522d0eb1e984
comstering <coms1101@naver.com>
2021-12-20 21:01:02 +0900

    [2621] Problem in BOJ solved code update

--- BOJ/Silver IV/악마 게임/devilGame.py
+++ BOJ/Silver IV/악마 게임/devilGame.py
@@ -9,23 +9,15 @@ for i in range(n):
     g = int(g)
     if len(m) >= len(w):
         continue
-    # 앞, 뒤에 추가되었을 경우
-    if m in w:
-        g /= (len(w) - len(m))
-        if g > result_g:
-            result = w
-            result_g = g
-    # 중간에 추가되었을 경우
-    else:
-        m_stack = list(reversed(m))
-        w_stack = list(w)
-        for i in w_stack:
-            if m_stack[-1] == i:
-                m_stack.pop()
-            if len(m_stack) == 0:
-                g /= (len(w) - len(m))
-                if g > result_g:
-                    result = w
-                    result_g = g
-                break
+    m_stack = list(reversed(m))
+    w_stack = list(w)
+    for j in w_stack:
+        if m_stack[-1] == j:
+            m_stack.pop()
+        if len(m_stack) == 0:
+            g /= (len(w) - len(m))
+            if g > result_g:
+                result = w
+                result_g = g
+            break
 print(result)
