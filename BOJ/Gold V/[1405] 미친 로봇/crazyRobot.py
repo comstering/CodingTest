03656611bdc33f54dbd966617bbbd2313d36a3a1
comstering <coms1101@naver.com>
2022-03-29 17:06:30 +0900

    Modified code [1405] problem in BOJ by using python

--- BOJ/Gold V/[1405] 미친 로봇/crazyRobot.py
+++ BOJ/Gold V/[1405] 미친 로봇/crazyRobot.py
@@ -5,30 +5,30 @@ class CrazyRobot:
         self.west = west
         self.south = south
         self.north = north
+        self.graph = [[False] * (2 * self.n + 1) for _ in range(2 * self.n + 1)]
 
-    def dfs(self, graph, nx, ny, now_per, next_per, n):
-        if graph[ny][nx]:  # 이미 지나간 위치일 경우 단순 경로가 아니므로 0반환
+    def dfs(self, nx, ny, now_per, next_per, n):
+        if self.graph[ny][nx]:  # 이미 지나간 위치일 경우 단순 경로가 아니므로 0반환
             return 0
         elif next_per == 0:  # 다음 방향으로 갈 가능성이 없는 것이므로 0반환
             return 0
         elif n == 0:  # 단순경로의 마지막까지 도달했을 경우 해당 위치까지의 확률 반환
             return now_per * next_per
         else:
-            graph[ny][nx] = True  # 현재 위치 도착 처리
+            self.graph[ny][nx] = True  # 현재 위치 도착 처리
             per = now_per * next_per  # 현재 위치까지 도달할 확률
             # 동서남북 모두 지나가기
             result = 0
-            result += self.dfs(graph, nx + 1, ny, per, self.east, n - 1)  # 동쪽
-            result += self.dfs(graph, nx - 1, ny, per, self.west, n - 1)  # 서쪽
-            result += self.dfs(graph, nx, ny + 1, per, self.south, n - 1)  # 남쪽
-            result += self.dfs(graph, nx, ny - 1, per, self.north, n - 1)  # 북쪽
+            result += self.dfs(nx + 1, ny, per, self.east, n - 1)  # 동쪽
+            result += self.dfs(nx - 1, ny, per, self.west, n - 1)  # 서쪽
+            result += self.dfs(nx, ny + 1, per, self.south, n - 1)  # 남쪽
+            result += self.dfs(nx, ny - 1, per, self.north, n - 1)  # 북쪽
             # 전부 탐색했을 경우 다음 탐색을 위해 다시 현재 위치 도착처리 취소
-            graph[ny][nx] = False
+            self.graph[ny][nx] = False
             return result
 
     def returnAnswer(self):
-        graph = [[False] * (2 * self.n + 1) for _ in range(2 * self.n + 1)]
-        return self.dfs(graph, self.n, self.n, 1, 1, self.n)
+        return self.dfs(self.n, self.n, 1, 1, self.n)
 
 
 n, east, west, south, north = map(int, input().split())
