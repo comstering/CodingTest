fb9f17e528ae4e8b0520c5a0664970e02d3e5c51
comstering <coms1101@naver.com>
2022-04-15 01:46:04 +0900

    Add comment [6198] problem in BOJ

--- BOJ/Gold V/[6198] 옥상 정원 꾸미기/roofGardenDecoration.kt
+++ BOJ/Gold V/[6198] 옥상 정원 꾸미기/roofGardenDecoration.kt
@@ -7,19 +7,25 @@ fun main() {
         buildings.add(readLine()!!.toLong())
     }
 
+    // 비교할 대상의 빌딩 리스트
     val nextBuildings: ArrayList<Array<Long>> = ArrayList<Array<Long>>()
     var count: Long = 0
 
     for(i in 1 until n) {
+        // 비교 대상 빌딩 리스트에 현재 빌딩 리스트의 마지막 빌딩 빼서 넣기
         nextBuildings.add(arrayOf(buildings.removeAt(buildings.size - 1), i))
-        while(!nextBuildings.isEmpty() && nextBuildings.last()[0] < buildings.last()) {
+        // 비교할 빌딩이 없을 때까지 반복
+        // 비교 대상 빌딩 리스트의 마지막 빌딩과 현재 빌딩 리스트의 마지막 빌딩 비교
+        while(nextBuildings.isNotEmpty() && nextBuildings.last()[0] < buildings.last()) {
+            // 비교 대상 빌딩이 더 작으면 비교 대상 빌딩 리스트에서 빼기
             nextBuildings.removeAt(nextBuildings.size - 1)
         }
-        if(nextBuildings.isEmpty()) {
-            count += i
-        }
-        else {
-            count += i - nextBuildings.last()[1]
+
+        count += when(nextBuildings.isEmpty()) {
+            // 비교할 빌딩이 남아있지 않다면 현재의 인덱스 더하기
+            true -> i
+            // 비교할 빌딩이 남아있으면 현재 인덱스에서 남아있는 비교 빌딩의 리스트 인덱스 뺀 값 더하기
+            false -> i - nextBuildings.last()[1]
         }
     }
 
--- BOJ/Gold V/[6198] 옥상 정원 꾸미기/roofGardenDecoration.py
+++ BOJ/Gold V/[6198] 옥상 정원 꾸미기/roofGardenDecoration.py
@@ -7,17 +7,21 @@ for _ in range(n):
     buildings.append(int(sys.stdin.readline()))
 
 count = 0
+# 비교할 대상의 빌딩 리스트
 next_buildings = []
 for i in range(1, n):
+    # 비교 대상 빌딩 리스트에 현재 빌딩 리스트의 마지막 빌딩 빼서 넣기
     next_buildings.append((buildings.pop(), i))
-    while next_buildings:
-        if next_buildings[-1][0] < buildings[-1]:
-            next_buildings.pop()
-        else:
-            break
+    # 비교할 빌딩이 없을 때까지 반복
+    # 비교 대상 빌딩 리스트의 마지막 빌딩과 현재 빌딩 리스트의 마지막 빌딩 비교
+    while next_buildings and (next_buildings[-1][0] < buildings[-1]):
+        # 비교 대상 빌딩이 더 작으면 비교 대상 빌딩 리스트에서 빼기
+        next_buildings.pop()
 
+    # 비교할 빌딩이 남아있으면 현재 인덱스에서 남아있는 비교 빌딩의 리스트 인덱스 뺀 값 더하기
     if next_buildings:
         count += i - next_buildings[-1][1]
+    # 비교할 빌딩이 남아있지 않다면 현재의 인덱스 더하기
     else:
         count += i
 
