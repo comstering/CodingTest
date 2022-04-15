a0817b23c3947dd98727420c66047dcb0cb1f82d
comstering <coms1101@naver.com>
2022-04-15 23:54:49 +0900

    Success [2338] problem in BOJ by using kotlin

---
+++ BOJ/Bronze V/[2338] 긴자리 계산/longNumberCalc.kt
@@ -0,0 +1,10 @@
+import java.math.BigInteger
+
+fun main() {
+    val a: BigInteger = readLine()!!.toBigInteger()
+    val b: BigInteger = readLine()!!.toBigInteger()
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+}
